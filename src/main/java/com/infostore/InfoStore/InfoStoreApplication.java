16eaa69465d2f4b6466b15c97b17f47ed8d543c9
Gustavo Orelio <oreliogustavo@gmail.com>
2022-05-27 20:50:06 -0300

    Iniciando projeto

---
+++ src/main/java/com/infostore/InfoStore/InfoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.infostore.InfoStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infostore/InfoStore/InfoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infostore.InfoStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
