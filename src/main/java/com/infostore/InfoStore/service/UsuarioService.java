469bc9ab40d27cb2b9c46a6984fe3e9294ba3291
Gustavo Orelio <oreliogustavo@gmail.com>
2022-07-02 19:26:43 -0300

    Adicionando validação de cpf e de email.

--- src/main/java/com/infostore/InfoStore/service/UsuarioService.java
+++ src/main/java/com/infostore/InfoStore/service/UsuarioService.java
@@ -18,9 +18,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Base64;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class UsuarioService {
+
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
+
     @Autowired
     private UsuarioRepository usuarioRepository;
     @Autowired
@@ -50,7 +58,9 @@ public class UsuarioService {
     }
 
     public Usuario save(Usuario usuario) throws BadResourceException, ResourceAlreadyExistsException {
-        if(!StringUtils.isEmpty(usuario.getNome())) {
+        if(!StringUtils.isEmpty(usuario.getNome()) && validarCPF(usuario.getCpf())) {
+            Matcher matcher  = pattern.matcher(usuario.getEmail());
+            if(matcher.matches()==false) usuario.setEmail("");
             usuario.setImagemPerfil(Base64.getEncoder().encode(usuario.getImagemPerfil()));;
             usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
             if (usuario.getId() != null && existsById(usuario.getId())) {
@@ -74,7 +84,9 @@ public class UsuarioService {
     }
 
     public void update(Usuario usuario) throws BadResourceException, ResourceNotFoundException {
-        if (!StringUtils.isEmpty(usuario.getNome())){
+        if(!StringUtils.isEmpty(usuario.getNome()) && validarCPF(usuario.getCpf())) {
+            Matcher matcher  = pattern.matcher(usuario.getEmail());
+            if(matcher.matches()==false) usuario.setEmail("");
             usuario.setImagemPerfil(Base64.getEncoder().encode(usuario.getImagemPerfil()));;
             usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
             if (!existsById(usuario.getId())){
@@ -100,6 +112,55 @@ public class UsuarioService {
 
     public Long count() {
         return usuarioRepository.count();
+    }
+
+    public static boolean validarCPF(String CPF) {
+
+        if (CPF.equals("00000000000") ||
+                CPF.equals("11111111111") ||
+                CPF.equals("22222222222") || CPF.equals("33333333333") ||
+                CPF.equals("44444444444") || CPF.equals("55555555555") ||
+                CPF.equals("66666666666") || CPF.equals("77777777777") ||
+                CPF.equals("88888888888") || CPF.equals("99999999999") ||
+                (CPF.length() != 11))
+            return(false);
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        try {
+            sm = 0;
+            peso = 10;
+            for (i=0; i<9; i++) {
+                num = (int)(CPF.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig10 = '0';
+            else dig10 = (char)(r + 48);
+
+            sm = 0;
+            peso = 11;
+            for(i=0; i<10; i++) {
+                num = (int)(CPF.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig11 = '0';
+            else dig11 = (char)(r + 48);
+
+            if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
+                return(true);
+            else return(false);
+        } catch (java.util.InputMismatchException erro) {
+            return(false);
+        }
     }
 
 }
