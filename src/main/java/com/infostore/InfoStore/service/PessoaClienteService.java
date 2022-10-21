a1492fbdfedd7d6ca5f62db8fdab4c736e38a560
Gustavo Orelio <oreliogustavo@gmail.com>
2022-10-21 00:56:13 -0300

    Email com template

--- src/main/java/com/infostore/InfoStore/service/PessoaClienteService.java
+++ src/main/java/com/infostore/InfoStore/service/PessoaClienteService.java
@@ -2,12 +2,13 @@ package com.infostore.InfoStore.service;
 
 import com.infostore.InfoStore.dto.PessoaClienteRequestDTO;
 import com.infostore.InfoStore.model.Pessoa;
-import com.infostore.InfoStore.repository.PermissaoRepository;
 import com.infostore.InfoStore.repository.PessoaClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 
 @Service
 public class PessoaClienteService {
@@ -15,7 +16,6 @@ public class PessoaClienteService {
     private PessoaClienteRepository pessoaRepository;
     @Autowired
     private PermissaoPessoaService permissaoPessoaService;
-
     @Autowired
     private EmailService emailService;
 
@@ -24,7 +24,11 @@ public class PessoaClienteService {
         pessoa.setDataCriacao(new Date());
         Pessoa pessoaNovo = pessoaRepository.saveAndFlush(pessoa);
         permissaoPessoaService.vincularPessoaPermissaoCliente(pessoaNovo);
-        emailService.enviarEmailTexto(pessoaNovo.getEmail(), "Cadastro na Loja Tabajara", "O registro na loja foi realizado com sucesso. Em breve você receberá a senha de acesso por e-mail!!!");
+//        emailService.enviarEmailTexto(pessoaNovo.getEmail(), "Cadastro na Loja Tabajara", "O registro na loja foi realizado com sucesso. Em breve você receberá a senha de acesso por e-mail!!!");
+        Map<String, Object> proprMap = new HashMap<>();
+        proprMap.put("nome", pessoaNovo.getNome());
+        proprMap.put("mensagem", "O registro na loja foi realizado com sucesso. Em breve você receberá a senha de acesso por e-mail!!!");
+        emailService.enviarEmailTemplate(pessoaNovo.getEmail(), "Cadastro na Loja Tabajara", proprMap);
         return pessoaNovo;
     }
 
